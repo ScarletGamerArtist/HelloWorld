a41a9099478add2149dd3656788afedff74cc747
alanna.botscharow <alanna.botscharow@CLC-LAB209-18.TCSD.tooelesd.org>
2018-09-04 13:18:52 -0600

    first commit

---
+++ src/AlannaBotscharow/Hello_World/HelloWorld.java
@@ -0,0 +1,22 @@
+/**
+ * hello world program prints hello world in console
+ */
+package AlannaBotscharow.Hello_World;
+
+/**
+ * @author alanna.botscharow
+ * 9/18
+ * hello world
+ *
+ */
+public class HelloWorld {
+
+	
+	
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		
+	}
+
+}
